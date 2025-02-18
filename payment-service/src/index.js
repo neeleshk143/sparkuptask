80fdd67cbf7f7c40a85ca0206b15491515f7a5b9
neeleshk342 <aneelesh342@gmail.com>
2025-02-18 23:56:44 +0530

    fix:minor

--- payment-service/src/index.js
+++ payment-service/src/index.js
@@ -8,7 +8,7 @@ app.use(express.json());
 mongoose.connect("mongodb://mongodb:27017/orders_db", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(() => {
         console.log(" Payment Service Connected to MongoDB");
-        app.listen(5000, () => console.log("🚀 Payment Service running on port 5000"));
+        app.listen(5000, () => console.log("Payment Service running on port 5000"));
 
         // Start consuming messages after connecting to MongoDB
         console.log("going to payment controller")
